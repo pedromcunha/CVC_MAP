19eb5cc55d2f2e914922ac5fdc05543c134f1693
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-09-12 03:27:14 -0400

    configured the css and some html tweaks

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -8,7 +8,28 @@
  *
  * Main module of the application.
  */
-angular
-  .module('frontendApp', [
-    'ngTouch'
-  ]);
+    angular
+    .module('frontendApp', [
+        'ngTouch',
+        'ngMaterial'
+    ])
+
+    //Theme Configuration
+    .config(function ($mdThemingProvider) {
+        //create color palletes
+        // var okraGreen = $mdThemingProvider.extendPalette('green', {
+        //     '500': '#99b742',
+        // });
+        // var okraGreenAccent = $mdThemingProvider.extendPalette('green', {
+        //     'A200': '#99b742'
+        // });
+
+        //define color extensions
+        // $mdThemingProvider.definePalette('okraGreen', okraGreen);
+        // $mdThemingProvider.definePalette('okraGreenAccent', okraGreenAccent);
+
+        //register extensions to the theme
+        $mdThemingProvider.theme('default')
+            .primaryPalette('pink')
+            .accentPalette('purple');
+    });
