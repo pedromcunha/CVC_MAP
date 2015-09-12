1895cf9007f42bc8a2a7b6baadf1e2746e4cf882
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-09-12 02:26:10 -0400

    add angular material and build

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -23,6 +23,9 @@ module.exports = function(config) {
     files: [
       // bower:js
       'bower_components/angular/angular.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-material/angular-material.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
