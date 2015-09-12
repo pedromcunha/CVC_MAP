eb88e76d5e66dc1b123231e691140625bf6a6db8
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-09-12 14:05:33 -0400

    compile after making the utils

---
+++ frontend/app/scripts/services/utils.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name cvcApp.utils
+ * @description
+ * # utils
+ * Service in the cvcApp.
+ */
+angular.module('cvcApp')
+  .service('utils', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
