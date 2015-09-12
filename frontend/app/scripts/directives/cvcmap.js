48fc30b885ae75c3994204a209700079fb9bca31
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-09-12 15:30:04 -0400

    getting the location information

--- frontend/app/scripts/directives/cvcmap.js
+++ frontend/app/scripts/directives/cvcmap.js
@@ -7,31 +7,37 @@
  * # cvcMap
  */
 angular.module('cvcApp')
-  .directive('cvcMap', function ($timeout) {
+  .directive('cvcMap', function ($timeout, utils, user) {
     return {
-      template: '<div id="map" layout-fill></div>',
-      restrict: 'E',
-      replace: true,
-      link: function () {
-          $timeout(function() {
+        template: '<div id="map" layout-fill></div>',
+        restrict: 'E',
+        replace: true,
+        link: function () {
+            $timeout(function() {
               window.cartodb.createVis('map', 'https://pedrocunha.cartodb.com/api/v2/viz/a4a0406e-5962-11e5-b251-0e49835281d6/viz.json')
               .done(function(vis, layers) {
-                  console.log(vis, layers);
-                  // layer 0 is the base layer, layer 1 is cartodb layer
-                  // when setInteraction is disabled featureOver is triggered
-                  layers[1].setInteraction(true);
-                  layers[1].on('featureOver', function(e, latlng, pos, data, layerNumber) {
-                    console.log(e, latlng, pos, data, layerNumber);
-                  });
-
-                  // you can get the native map to work with it
-                  var map = vis.getNativeMap();
-
-                  // now, perform any operations you need, e.g. assuming map is a L.Map object:
-                  map.setZoom(3);
-                  map.panTo([50.5, 30.5]);
+                    utils.getZipCode().then(function() {
+                        visualizeMap(vis, layers);
+                    });
                 });
-          });
-      }
+            });
+
+            function visualizeMap(vis, layers) {
+                // layer 0 is the base layer, layer 1 is cartodb layer
+                // when setInteraction is disabled featureOver is triggered
+                layers[1].setInteraction(true);
+                layers[1].on('featureOver', function(e, latlng, pos, data, layerNumber) {
+                    console.log(e, latlng, pos, data, layerNumber);
+                });
+
+                // you can get the native map to work with it
+                var map = vis.getNativeMap();
+
+                // now, perform any operations you need, e.g. assuming map is a L.Map object:
+                if(user.location.loc) {
+                    map.setView(user.location.loc.split(','));
+                }
+            }
+        }
     };
   });
--- frontend/app/scripts/services/utils.js
+++ frontend/app/scripts/services/utils.js
@@ -8,6 +8,17 @@
  * Service in the cvcApp.
  */
 angular.module('cvcApp')
-  .service('utils', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+    .service('utils', function ($http, user) {
+        var utils = {};
+
+        utils.getZipCode = function() {
+            return $http.get("http://ipinfo.io")
+                .then(function(response) {
+                    if(response && response.data) {
+                        user.location = response.data;
+                    }
+                });
+        };
+
+        return utils;
+    });
