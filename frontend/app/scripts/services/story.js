4acab55f723aa903bbd4a092a6fd10fcdf1804e8
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-09-12 21:28:45 -0400

    fixed the dialog

--- frontend/app/scripts/services/story.js
+++ frontend/app/scripts/services/story.js
@@ -8,15 +8,38 @@
  * Service in the cvcApp.
  */
 angular.module('cvcApp')
-    .factory('story', function () {
+    .factory('story', function ($mdDialog) {
         var story = {};
 
         story.get = function(storyId) {
             console.log(storyId);
+
+            return {};
         };
 
         story.create = function() {
 
+        };
+
+        story.dialog = function(storyId) {
+            if(storyId) {
+                story.currentStory = story.get(storyId);
+            } else {
+                story.currentStory = story.get();
+            }
+
+            $mdDialog.show({
+                controller: 'storyModalCtrl',
+                templateUrl: 'views/story_modal_dialog.html',
+                parent: angular.element(document.body),
+                // targetEvent: $event,
+                clickOutsideToClose:true
+            })
+            .then(function(response) {
+                console.log(response);
+            }, function() {
+                console.log('closed by backdrop!');
+            });
         };
 
         return story;
